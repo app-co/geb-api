b58f9e3cef87512fc6225e8e1cba395083eb2c3b
app-c <william@app-com.digital>
2022-12-12 04:57:51 -0300

    add csv estera

--- src/modules/posts/http/routes/post.routes.ts
+++ src/modules/posts/http/routes/post.routes.ts
@@ -45,6 +45,104 @@ post.post('/csv', up.single('csv'), (req, res) => {
    });
 });
 
+post.post('/csv-estera', up.single('csv'), (req, res) => {
+   const dt: any[] = [];
+
+   const { file } = req;
+   return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file!.path);
+      const parseFile = parse({ delimiter: ';' });
+
+      stream.pipe(parseFile);
+
+      parseFile
+         .on('data', line => {
+            const [
+               Nota,
+               Tipo,
+               Descricao_da_nota,
+               TAM,
+               Depart,
+               Divisão,
+               Bop,
+               Cidade,
+               Distribuidora,
+               Fábrica,
+               Status,
+               Código,
+               Texto_cod_medida,
+               Texto_das_medidas,
+               Dt_Criação,
+               Dt_programação,
+               Local_inst,
+               Lati,
+               Long,
+               Alimentador,
+               Conjunto_elétrico,
+               Qtde_clientes,
+               CHI_max,
+               Obra_livre,
+               Nota_pai,
+               Possui_DI,
+               Num_DI,
+               Possui_viab,
+               Data_viab,
+               Dt_Empreita,
+               Mês_empreita,
+               Ano_empreita,
+               Km,
+               MO,
+               CAPEX,
+               equipe,
+            ] = line;
+            dt.push({
+               Nota,
+               Tipo,
+               Descricao_da_nota,
+               TAM,
+               Depart,
+               Divisão,
+               Bop,
+               cidade: Cidade,
+               Distribuidora,
+               Fábrica,
+               Status,
+               Código,
+               Texto_cod_medida,
+               TLE: Texto_das_medidas,
+               Dt_Criação,
+               Dt_programação,
+               Local_inst,
+               Lati,
+               Long,
+               Alimentador,
+               Conjunto_elétrico,
+               Qtde_clientes,
+               CHI_max,
+               Obra_livre,
+               Nota_pai,
+               Possui_DI,
+               Num_DI,
+               Possui_viab,
+               Data_viab,
+               Dt_Empreita,
+               Mês_empreita,
+               Ano_empreita,
+               Km,
+               MO,
+               CAPEX,
+               equipe,
+            });
+         })
+         .on('end', () => {
+            resolve(res.json(dt));
+         })
+         .on('error', err => {
+            reject(err);
+         });
+   });
+});
+
 post.use(Auth);
 
 post.post('/', controller.create);
